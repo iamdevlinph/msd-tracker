759307a62cee10182387c81292163beba40ecb43
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-08 17:02:12 +0800

    Regions scrollable horizontal

--- src/components/monster-codex/components/regions.tsx
+++ src/components/monster-codex/components/regions.tsx
@@ -4,6 +4,7 @@ import { REGIONS_DATA } from "@/components/monster-codex/data/REGIONS_DATA";
 import type { MonsterCodexRegion } from "@/components/monster-codex/store/monster-codex-constants";
 import { useMonsterCodexStore } from "@/components/monster-codex/store/monster-codex-store";
 import { Card, CardContent } from "@/components/ui/card";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 
 export const Regions = () => {
@@ -17,31 +18,34 @@ export const Regions = () => {
 	}, [filterCodex, activeRegion]);
 
 	return (
-		<div className="flex flex-row">
-			{REGIONS_DATA.map((region) => {
-				return (
-					<Card
-						key={region.id}
-						className={cn(
-							"cursor-pointer",
-							"bg-background border-background opacity-50",
-							activeRegion === region.region &&
-								"bg-card border-initial border-b-0 rounded-b-none shadow-none opacity-100",
-						)}
-						onClick={() => setActiveRegion(region.region)}
-					>
-						<CardContent className="w-32 flex flex-col justify-center items-center">
-							<img
-								src={region.image}
-								width="50"
-								height="50"
-								alt={`${region.region} map icon`}
-							/>
-							<h1>{toSentenceCase(region.region)}</h1>
-						</CardContent>
-					</Card>
-				);
-			})}
-		</div>
+		<ScrollArea className="">
+			<div className="flex flex-row">
+				{REGIONS_DATA.map((region) => {
+					return (
+						<Card
+							key={region.id}
+							className={cn(
+								"cursor-pointer",
+								"bg-background border-background opacity-50",
+								activeRegion === region.region &&
+									"bg-card border-initial border-b-0 rounded-b-none shadow-none opacity-100",
+							)}
+							onClick={() => setActiveRegion(region.region)}
+						>
+							<CardContent className="w-32 flex flex-col justify-center items-center">
+								<img
+									src={region.image}
+									width="50"
+									height="50"
+									alt={`${region.region} map icon`}
+								/>
+								<h1>{toSentenceCase(region.region)}</h1>
+							</CardContent>
+						</Card>
+					);
+				})}
+			</div>
+			<ScrollBar orientation="horizontal" />
+		</ScrollArea>
 	);
 };
