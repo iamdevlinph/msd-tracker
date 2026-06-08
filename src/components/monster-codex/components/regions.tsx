6b61645e95af41e42adc0afe6c4f6688a1395b09
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-08 15:55:51 +0800

    Region icons

--- src/components/monster-codex/components/regions.tsx
+++ src/components/monster-codex/components/regions.tsx
@@ -1,19 +1,16 @@
 import { toSentenceCase } from "common-utils-pkg";
 import { useEffect, useState } from "react";
 import { REGIONS_DATA } from "@/components/monster-codex/data/REGIONS_DATA";
+import type { MonsterCodexRegion } from "@/components/monster-codex/store/monster-codex-constants";
 import { useMonsterCodexStore } from "@/components/monster-codex/store/monster-codex-store";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-
-const REGIONS = REGIONS_DATA.map((item) => item.region);
-
-export type MonsterCodexRegions = (typeof REGIONS)[number];
 
 export const Regions = () => {
 	const filterCodex = useMonsterCodexStore((s) => s.filterCodex);
 
 	const [activeRegion, setActiveRegion] =
-		useState<(typeof REGIONS)[number]>("elendor");
+		useState<MonsterCodexRegion>("elendor");
 
 	useEffect(() => {
 		filterCodex({ region: activeRegion });
@@ -21,21 +18,26 @@ export const Regions = () => {
 
 	return (
 		<div className="flex flex-row">
-			{REGIONS.map((region) => {
+			{REGIONS_DATA.map((region) => {
 				return (
 					<Card
-						key={region}
+						key={region.id}
 						className={cn(
 							"cursor-pointer",
-							"bg-background border-background ",
-							activeRegion === region &&
-								"bg-card border-initial border-b-0 rounded-b-none shadow-none",
+							"bg-background border-background opacity-50",
+							activeRegion === region.region &&
+								"bg-card border-initial border-b-0 rounded-b-none shadow-none opacity-100",
 						)}
-						onClick={() => setActiveRegion(region)}
+						onClick={() => setActiveRegion(region.region)}
 					>
-						<CardContent className="text-center w-32">
-							<h1>{toSentenceCase(region)}</h1>
-							<small>{"82.6%"}</small>
+						<CardContent className="w-32 flex flex-col justify-center items-center">
+							<img
+								src={region.image}
+								width="50"
+								height="50"
+								alt={`${region.region} map icon`}
+							/>
+							<h1>{toSentenceCase(region.region)}</h1>
 						</CardContent>
 					</Card>
 				);
--- src/components/monster-codex/data/REGIONS_DATA.ts
+++ src/components/monster-codex/data/REGIONS_DATA.ts
@@ -9,25 +9,37 @@ export const REGIONS_DATA = [
 	{
 		id: REGION_ELENDOR,
 		region: "elendor",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-elendor.png",
 	},
 	{
 		id: REGION_VARHINE,
 		region: "varhine",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-varhine.png",
 	},
 	{
 		id: REGION_SERENIA,
 		region: "serenia",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-serenia.png",
 	},
 	{
 		id: 4,
 		region: "surah",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-surah.png",
 	},
 	{
 		id: REGION_NAMRYUNG,
 		region: "namryung",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-namryung.png",
 	},
 	{
 		id: REGION_MUWON,
 		region: "muwon",
+		image:
+			"https://res.cloudinary.com/dfrhytey3/image/upload/v1780904899/msd/Maps/map-icon-muwon.png",
 	},
 ] as const;
