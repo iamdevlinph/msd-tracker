e0061a26e595f87c22ff51d5d873241c843bf020
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-08 17:12:15 +0800

    Try to deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
 		devtools(),
-		cloudflare({
-			viteEnvironment: {
-				name: "ssr",
-				//  childEnvironments: ["rsc"]
-			},
-		}),
+		// cloudflare({
+		// 	viteEnvironment: {
+		// 		name: "ssr",
+		// 		//  childEnvironments: ["rsc"]
+		// 	},
+		// }),
 		tailwindcss(),
 		tanstackStart({
 			// rsc: {
 			// 	enabled: true,
 			// },
 		}),
-		// nitro(),
+		nitro(),
 		// rsc(),
 		viteReact(),
 	],
