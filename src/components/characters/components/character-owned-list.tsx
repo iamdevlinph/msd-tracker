bd514e0f884e191777bd1b3275450d9da0fe2cd2
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-11 05:48:37 +0800

    wip - edit owned char details

--- src/components/characters/components/character-owned-list.tsx
+++ src/components/characters/components/character-owned-list.tsx
@@ -1,10 +1,26 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import CharacterCard from "@/components/characters/components/character-card";
+import { CharacterDetailsForm } from "@/components/characters/components/character-details-form";
+import { CharacterPortrait } from "@/components/characters/components/character-portrait";
 import { useCharacterFilter } from "@/components/characters/store/characters-filter-store";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
 import { CHARACTERS_DATA } from "@/data/CHARACTERS_DATA";
+import { cn } from "@/lib/utils";
 import { useAppStore } from "@/stores/app-store";
 
 export const CharacterOwnedList = () => {
+	const [open, setOpen] = useState(false);
+	const [charToAdd, setCharToAdd] = useState<null | number>(null);
+	const hasSelectedChar = !!charToAdd;
+
+	const charToAddInfo = hasSelectedChar ? CHARACTERS_DATA[charToAdd] : null;
+
 	const characterFilters = useCharacterFilter((s) => s.characterFilters);
 	const charactersOwned = useAppStore((s) => s.charactersOwned);
 
@@ -46,19 +62,67 @@ export const CharacterOwnedList = () => {
 			>
 				{enrichedCharacters.length > 0 &&
 					enrichedCharacters.map((charOwned) => (
-						<CharacterCard
+						<button
 							key={charOwned.id}
-							portraitSize={130}
-							iconSize={30}
-							portraitImage={charOwned.info.portraitImage}
-							name={charOwned.info.name}
-							element_id={charOwned.info.element_id}
-							class_id={charOwned.info.class_id}
-							tier={charOwned.info.tier}
-							awakening={charOwned.awakening}
-						/>
+							type="button"
+							onClick={() => {
+								setOpen(true);
+								setCharToAdd(charOwned.id);
+							}}
+						>
+							<CharacterCard
+								key={charOwned.id}
+								portraitSize={130}
+								iconSize={30}
+								portraitImage={charOwned.info.portraitImage}
+								name={charOwned.info.name}
+								element_id={charOwned.info.element_id}
+								class_id={charOwned.info.class_id}
+								tier={charOwned.info.tier}
+								awakening={charOwned.awakening}
+							/>
+						</button>
 					))}
 			</div>
+
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogContent
+					className={cn(
+						"overflow-y-scroll max-h-screen",
+						"max-w-sm sm:min-w-min lg:min-w-218",
+						hasSelectedChar && "lg:min-w-max",
+						"h-[calc(100dvh-50px)] lg:h-min",
+					)}
+					onCloseAutoFocus={() => setCharToAdd(null)}
+				>
+					<DialogHeader>
+						<DialogTitle>
+							{!hasSelectedChar && "Add Character"}
+							{hasSelectedChar && charToAddInfo && (
+								<div className="flex gap-5 items-center">
+									<div className="flex items-center gap-2 relative">
+										<CharacterPortrait
+											portraitImg={charToAddInfo.portraitImage}
+											portraitSize={50}
+											tier={charToAddInfo.tier}
+										/>
+										<span>{charToAddInfo.name}</span>
+									</div>
+								</div>
+							)}
+						</DialogTitle>
+						<DialogDescription></DialogDescription>
+					</DialogHeader>
+					<div className="">
+						{hasSelectedChar && (
+							<CharacterDetailsForm
+								char_id={charToAdd}
+								onClose={() => setOpen(false)}
+							/>
+						)}
+					</div>
+				</DialogContent>
+			</Dialog>
 		</>
 	);
 };
