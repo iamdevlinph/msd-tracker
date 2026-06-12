c29b213b9402634bf6b00a112ef5d8e49dd42769
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-12 22:04:03 +0800

    Remove in progress code

--- src/components/monsterlings/components/monsterling-card.tsx
+++ src/components/monsterlings/components/monsterling-card.tsx
@@ -1,45 +1,45 @@
-import { MONSTERLING_CARD_WIDTH } from "@/components/monsterlings/components/monsterling-constants";
-import { TierPortrait } from "@/components/shared/tier-portrait";
-import { MONSTERLINGS_DATA } from "@/data/MONSTERLINGS_DATA";
-import { cn } from "@/lib/utils";
-import type { MonsterlingOwned } from "@/stores/monsterlings-slice";
+// import { MONSTERLING_CARD_WIDTH } from "@/components/monsterlings/components/monsterling-constants";
+// import { TierPortrait } from "@/components/shared/tier-portrait";
+// import { MONSTERLINGS_DATA } from "@/data/MONSTERLINGS_DATA";
+// import { cn } from "@/lib/utils";
+// import type { MonsterlingOwned } from "@/stores/monsterlings-slice";
 
-export const MonsterlingCard = ({
-	monsterling_id,
-	tier_id,
-	traits,
-}: MonsterlingOwned) => {
-	const { name, image, id } = MONSTERLINGS_DATA[monsterling_id];
+// export const MonsterlingCard = ({
+// 	monsterling_id,
+// 	tier_id,
+// 	traits,
+// }: MonsterlingOwned) => {
+// 	const { name, image, id } = MONSTERLINGS_DATA[monsterling_id];
 
-	return (
-		<div
-			className={cn(
-				"grid bg-card gap-y-2 gap-x-0 rounded-lg",
-				`w-[${MONSTERLING_CARD_WIDTH}px]`,
-			)}
-			style={{
-				gridTemplateAreas: "'portrait name' 'portrait stats'",
-			}}
-		>
-			<small className="text-center" style={{ gridArea: "name" }}>
-				{name}
-			</small>
-			<div className="relative" style={{ gridArea: "portrait" }}>
-				<TierPortrait
-					tier={tier_id}
-					portraitImg={image}
-					portraitSize={120}
-					name={name}
-					hideTierBg
-				/>
-			</div>
+// 	return (
+// 		<div
+// 			className={cn(
+// 				"grid bg-card gap-y-2 gap-x-0 rounded-lg",
+// 				`w-[${MONSTERLING_CARD_WIDTH}px]`,
+// 			)}
+// 			style={{
+// 				gridTemplateAreas: "'portrait name' 'portrait stats'",
+// 			}}
+// 		>
+// 			<small className="text-center" style={{ gridArea: "name" }}>
+// 				{name}
+// 			</small>
+// 			<div className="relative" style={{ gridArea: "portrait" }}>
+// 				<TierPortrait
+// 					tier={tier_id}
+// 					portraitImg={image}
+// 					portraitSize={120}
+// 					name={name}
+// 					hideTierBg
+// 				/>
+// 			</div>
 
-			<div className="flex flex-col" style={{ gridArea: "stats" }}>
-				<small>Ice dmg 5%</small>
-				<small>Elemental weakness boost dmg 5%</small>
-				<small>Ice dmg 5%</small>
-				<small>Ice dmg 5%</small>
-			</div>
-		</div>
-	);
-};
+// 			<div className="flex flex-col" style={{ gridArea: "stats" }}>
+// 				<small>Ice dmg 5%</small>
+// 				<small>Elemental weakness boost dmg 5%</small>
+// 				<small>Ice dmg 5%</small>
+// 				<small>Ice dmg 5%</small>
+// 			</div>
+// 		</div>
+// 	);
+// };
--- src/components/monsterlings/components/monsterlings-list.tsx
+++ src/components/monsterlings/components/monsterlings-list.tsx
@@ -1,7 +1,4 @@
-import { MonsterlingCard } from "@/components/monsterlings/components/monsterling-card";
 import { MONSTERLING_CARD_WIDTH } from "@/components/monsterlings/components/monsterling-constants";
-import { STAT_ID_BY_STAT } from "@/data/STAT_DATA";
-import { TIER_ID_BY_TIER } from "@/data/TIERS_DATA";
 
 export const MonsterlingsList = () => {
 	return (
@@ -12,7 +9,7 @@ export const MonsterlingsList = () => {
 					gridTemplateColumns: `repeat(auto-fit, ${MONSTERLING_CARD_WIDTH}px)`,
 				}}
 			>
-				{Array.from({ length: 10 }).map((_val, idx) => {
+				{/* {Array.from({ length: 10 }).map((_val, idx) => {
 					const tempId = idx + 1;
 					return (
 						<MonsterlingCard
@@ -27,7 +24,7 @@ export const MonsterlingsList = () => {
 							]}
 						/>
 					);
-				})}
+				})} */}
 			</div>
 		</div>
 	);
